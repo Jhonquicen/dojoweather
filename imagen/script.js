04064c208ce128a4a797045f00cd251df50a913b
jhon quicen <qjferney1013@gmail.com>
2022-06-24 10:50:42 -0500

    temperatura

---
+++ imagen/script.js
@@ -0,0 +1,8 @@
+function eliminaCookie(){
+    var elementoCookie=document.querySelector(".cookie");//document.queryselector coje el elemento "cookie"
+    elementoCookie.remove();
+}
+
+function loading();{
+    alert("loading weather report of "+city+" ...")
+}
